ea34f9b6d6b776210118cfeaddf600efa555b5eb
ericvyc <eric_vilar2007@hotmail.com>
2018-07-06 08:55:16 -0400

    Correcao para sprng 2.0

--- src/main/java/com/eric/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/eric/pontointeligente/api/PontoInteligenteApplication.java
@@ -3,8 +3,15 @@ package com.eric.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.eric.pontointeligente.api.repositories.LancamentoRepository;
+
+import controllers.CadastroPJController;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@ComponentScan(basePackageClasses = CadastroPJController.class)
+@ComponentScan(basePackageClasses = LancamentoRepository.class)
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
